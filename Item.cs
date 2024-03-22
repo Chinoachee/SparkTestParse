65717d487bbef74e36a82f6ef720f05bdf47e097
Chinoachee <eskeritasd@gmail.com>
2024-03-23 00:15:25 +0500

    just a test

---
+++ Item.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SparkParserTest
+{
+    internal class Item
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public Item(string name, decimal price)
+        {
+            Name = name;
+            Price = price;
+        }
+        public override string ToString()
+        {
+            return Name + ";" + Price;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using HtmlAgilityPack;
+using System.Text;
+
+namespace SparkParserTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HtmlDocument document = new HtmlDocument();
+            document.Load("C:\\Users\\anast\\Downloads\\Luk.html");
+            HtmlNode titleNode = document.DocumentNode.SelectSingleNode("//h1[@id='pagetitle']");
+            HtmlNode priceNode = document.DocumentNode.SelectSingleNode("//span[@class='price_value']");
+            string nameItem = titleNode.InnerText != null ? titleNode.InnerText : "None";
+            string priceItem = priceNode.InnerText;
+            Console.WriteLine(nameItem);
+            Console.WriteLine(priceItem);
+            Item item = new Item(nameItem, Convert.ToDecimal(priceItem));
+            using(StreamWriter sw = new StreamWriter("ЛукВкусный.csv", false, Encoding.UTF8))
+            {
+                sw.WriteLine(item   );
+            }
+        }
+    }
+}
